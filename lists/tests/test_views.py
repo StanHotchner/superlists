2c74b517720c73c9cce5014d24f010ab99f366a3
Stan Hotchner <stan.shih@sunplus-prof.com>
2017-03-06 14:19:00 +0800

    拆分单元测试到两个分开的文件中

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,7 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
-from django.template.loader import render_to_string
 from lists.models import Item, List
 from lists.views import home_page
 
@@ -43,37 +42,6 @@ class HomePageTest(TestCase):
         self.assertIn('itemey 1', response.content.decode())
         self.assertIn('itemey 2', response.content.decode())
      '''
-     
-class ListAndItemModelTest(TestCase):
-    # ---- 测试能否正确的保存数据到数据库并且读回 ----
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-        
-        # 创建第一个条目并保存
-        first_item = Item()
-        first_item.text = 'The first(ever) list item'
-        first_item.list = list_
-        first_item.save()
-        
-        # 创建第二个条目并保存
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-        
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-        
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2) #已经保存的条目应该要有2个
-        
-        first_saved_item = saved_items[0] # 从已经被保存的条目集中取出第1个
-        second_saved_item = saved_items[1] # 从已经被保存的条目集中取出第2个
-        self.assertEqual(first_saved_item.text, 'The first(ever) list item') #断言第 1 笔条目的字符应该是...
-        self.assertEqual(first_saved_item.list, list_) #断言已保存的第 1 笔条目所属的 list 应该要与前面创建的一致
-        self.assertEqual(second_saved_item.text, 'Item the second') #断言第 2 笔条目的字符应该是...
-        self.assertEqual(second_saved_item.list, list_) #断言已保存的第 1 笔条目所属的 list 应该要与前面创建的一致
         
 class ListViewTest(TestCase):
     def test_use_list_template(self):
