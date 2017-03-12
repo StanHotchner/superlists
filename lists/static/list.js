55e923b61f0383feea5b28caa43982311798f439
Stan Hotchner <stan.shih@sunplus-prof.com>
2017-03-12 21:49:27 +0800

    page 223

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,3 +1,5 @@
-$('input').on('keypress', function(){
-	$('.has-error').hide();
+jQuery(document).ready(function(){
+	jQuery('input').on('keypress', function(){
+		jQuery('.has-error').hide();
+	});
 });
